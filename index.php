c566d2772fdb810eb2ebc413c023a67c607991e9
Vivin Lukose <vivinlukose@Vivins-MacBook-Air.local>
2017-01-20 14:40:39 -0800

    sociallinks

--- index.php
+++ index.php
@@ -31,43 +31,26 @@
 <?php include ('partials/header.php') ?>
 <!--number block-->
 <div class="bgimg-5">
-<div class="container-fluid Numberblock">
-  <div class="MaxBlock">
-    <div class="NoSmall"><span id="ctl00_ContentPlaceHolder1_ProjectC" class="no">1225</span><br />User Interface</div>
-    <div class="NoSmall"><span id="ctl00_ContentPlaceHolder1_ClientC" class="no">800</span><br />Happy Clients</div>
-    <div class="NoSmall"><span id="ctl00_ContentPlaceHolder1_ProductC" class="no">25</span><br />Our Products</div>
-    <div class="NoSmall"><span id="ctl00_ContentPlaceHolder1_YearC" class="no">6</span><br />Experince Years</div>
-    <div class="Clear"></div>
+  <div class="container-fluid Numberblock">
+    <div class="MaxBlock">
+      <div class="NoSmall"><span id="ctl00_ContentPlaceHolder1_ProjectC" class="no">1225</span><br />User Interface</div>
+      <div class="NoSmall"><span id="ctl00_ContentPlaceHolder1_ClientC" class="no">800</span><br />Happy Clients</div>
+      <div class="NoSmall"><span id="ctl00_ContentPlaceHolder1_ProductC" class="no">25</span><br />Our Products</div>
+      <div class="NoSmall"><span id="ctl00_ContentPlaceHolder1_YearC" class="no">6</span><br />Experince Years</div>
+      <div class="Clear"></div>
+    </div>
   </div>
-</div>
-</div>
-<!-- Container (About Section) -->
-<?php include ('partials/about.php') ?>
+  </div>
+  <!-- Container (About Section) -->
+  <?php include ('partials/about.php') ?>
 
-<?php include ('partials/skills.php') ?>
+  <?php include ('partials/skills.php') ?>
 
-<!-- Container (Portfolio Section) -->
-
-
-<?php include ('partials/portfolio.php') ?>
-
-
-<?php include ('partials/contact.php') ?>
+  <!-- Container (Portfolio Section) -->
+  <?php include ('partials/portfolio.php') ?>
+  <?php include ('partials/contact.php') ?>
 <!-- Footer -->
-<footer class="container-fluid text-center bg-gray">
-  <a href="#myPage" title="To Top">
-    <span class="glyphicon glyphicon-chevron-up"></span>
-  </a>
-  <div class="w3-xlarge w3-section">
-    <a href="https://www.facebook.com/vivin.luka"><i class="fa fa-facebook-official w3-hover-text-indigo"></i></a>
-    <i class="fa fa-instagram w3-hover-text-purple"></i>
-    <a href="#"><i class="fa fa-twitter w3-hover-text-light-blue"></i></a>
-    <a href="https://www.linkedin.com/in/vivin-lukose-56909a1b?trk=nav_responsive_tab_profile"<i class="fa fa-linkedin w3-hover-text-indigo"></i></a>
-  </div>
-
-
-  <p>Designed by Vivin <a href="#" title="VivProtfolio">Protfolio </a></p>
-</footer>
+  <?php include ('partials/footer.php') ?>
 
 <script src="js/jquery.mixitup.js"></script>
 <script src="js/creative.js"></script>
---
+++ partials/footer.php
@@ -0,0 +1,15 @@
+<footer class="container-fluid text-center bg-gray">
+  <a href="#myPage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+  <div class="w3-xlarge social-links">
+    <a href="https://www.facebook.com/vivin.luka" target="_blank"><i class="fa fa-facebook-official w3-hover-text-indigo"></i></a>
+    <a href="https://github.com/vluka2003" target="_blank"><i class="fa fa-github w3-hover-text-purple"></i></a>
+    <a href="#"><i class="fa fa-twitter w3-hover-text-light-blue"></i></a>
+    <a href="#"><i class="fa fa-google-plus w3-hover-text-light-blue"></i></a>
+    <a href="https://www.linkedin.com/in/vivin-lukose-56909a1b?trk=nav_responsive_tab_profile" target="_blank"><i class="fa fa-linkedin w3-hover-text-indigo"></i></a>
+  </div>
+
+
+  <p>Designed by Vivin <a href="#" title="VivProtfolio">Protfolio </a></p>
+</footer>
